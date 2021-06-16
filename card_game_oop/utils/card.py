5665c3400813f3bc5a34a3436b3c8ae1df49f381
tcrasset <tomcrasset@protonmail.com>
2021-06-16 14:22:53 +0200

    Add coaches notes

--- card_game_oop/utils/card.py
+++ card_game_oop/utils/card.py
@@ -1,11 +1,12 @@
 class Card:
 
     def __init__(self, color, composition, name, points):
+        # COACHES' NOTES : Nice use of private names
         self._color = color
         self._composition = composition
         self._name = name
         self._points = points
-
+    
     def __call__(self, *args, **kwargs):
         return f"new card: {self._color} {self._composition} {self._name} {self._points} was created"
 
@@ -15,6 +16,10 @@ class Card:
     def __repr__(self):
         return f'[{self._color} {self._composition} {self._name}: {self._points}]'
 
+    # COACHES' NOTES : Usually the @property decorator is for single properties, not for
+    # a group of properties. https://www.freecodecamp.org/news/python-property-decorator/
     @property
     def get_card_properties(self):
         return self._color, self._composition, self._name, self._points
+
+# COACHES' NOTES : Symbol class missing
